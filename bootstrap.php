124eabbd6c69a1fe3412395e47d0f1f45c8fe729
justjavac <justjavac@gmail.com>
2015-09-20 15:03:55 +0800

    重新规划目录结构

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,36 @@
 <?php
 
-// Require the extension's composer autoload file. This will enable all of our
-// classes in the src directory to be autoloaded.
-require __DIR__.'/vendor/autoload.php';
+/**
+ * 简体中文
+ * 
+ * @package   justjavac\Flarum\SimpleChinese
+ * @copyright Copyright (c) 2015 The justjavac Authors
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+namespace justjavac\Flarum\SimpleChinese;
 
-// Return the name of our Extension class. Flarum will register it as a service
-// provider, allowing it to register bindings and execute code when the
-// application boots.
-return 'justjavac\Extension';
+use Flarum\Support\Extension as BaseExtension;
+use Illuminate\Events\Dispatcher;
+use Flarum\Events\RegisterLocales;
+
+class Extension extends BaseExtension
+{
+    public function listen(Dispatcher $events)
+    {
+        $events->listen(RegisterLocales::class, function (RegisterLocales $event) {
+            $event->manager->addLocale('zh-CN', '中文简体');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/core.yml');
+            $event->manager->addJsFile('zh-CN', __DIR__.'/locale/core.js');
+            $event->manager->addConfig('zh-CN', __DIR__.'/locale/core.php');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/likes.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/lock.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/mentions.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/pusher.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/sticky.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/subscriptions.yml');
+            $event->addTranslations('zh-CN', __DIR__.'/locale/tags.yml');
+        });
+    }
+}
+
+return __NAMESPACE__ . '\\Extension';
